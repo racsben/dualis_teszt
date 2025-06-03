460a312dcb63809d41d7f87066429c1ea00c6a2d
Cooox1 <81319187+Cooox1@users.noreply.github.com>
2025-06-03 21:45:09 +0200

    06_03

--- kriptográfia.py
+++ kriptográfia.py
@@ -1,31 +1,37 @@
-uzenet = 'helloworld'
-abc = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", " "]
+index=[ 0 , 1 , 2 ,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
-betu_indexre = dict(zip(abc, range(len(abc))))
-index_beture = dict(zip(range(len(abc)), abc))
+uzenet = input("Adjon meg egy Üzenetet: ")   #"helloworld" 
 
-karakterek = []
-for i in range(26):
-    kod = chr(ord('a')+i)
-    karakterek.append(kod)
-#for karakter in karakterek:
-    #print(karakter, end=",")
+kulcs = input("Adjon meg egy kulcsot: ")    #"abcdefgijkl"
+if len(kulcs) != len(uzenet):
+    kulcs = input("Adjon meg egy kulcsot, egyenlő hossúságut az üzenettel: ")
 
-def rejtjelezes(uzenet, kulcs):
-    #kulcsnak legalább akkorának kell lennie mint az üzenetnek
-    #rejtjelezett üzenet n. karaktere az üzenet n. karakter kódja + kulcs n. karakter kódja
-    #ha az eredmény nagyobb mint 26, akkor az eredmény a 27-el való osztás maradéka
-    #ha a kulcs hosszabb mint az üzenet, akkor a rejtjeles üzenet hossza az üzenet hossza lesz
-    kodolt = ""
+uzenet_szam = [] 
+kulcs_szam = []
 
-    darabolas_uzenet = [uzenet[i:i + len(kulcs)] for i in range(0,len(uzenet), len(kulcs))]
-    for darab in darabolas_uzenet:
-        i = 0
-        for betu in darab:
-            szam = (betu_indexre[betu] + betu_indexre[kulcs[i]]) % len(abc)
-            kodolt += index_beture[szam]
-            i += 1
-    
+def kodolas(uzenet, kulcs):
+    #kigyűjtöm a bekért adatok karaktereihez rendelt számot
+    for u, k in zip(uzenet,kulcs):
+        for betu,szam in zip(abc,index):
+            if u == betu:
+                uzenet_szam.append(szam)
+            if k == betu:
+                kulcs_szam.append(szam)
+
+    #kód kialakítása
+    kodolt = "" #hfnosauzun 
+    kodolt_szam = [] 
+    lista_hossza = len(index)
+    for u, k in zip(uzenet_szam, kulcs_szam):
+        kodolt_kar_szam = (u+k) % lista_hossza
+        kodolt_szam.append(kodolt_kar_szam)
+
+    for k in kodolt_szam:
+        for betu,szam in zip(abc,index):
+            if k == szam:
+                kodolt += betu
+
     return kodolt
 
-print(rejtjelezes('helloworld', "abcdefgijkl"))
+print(kodolas(uzenet,kulcs))
