1be79ad3944550f46a19f7698c508ef22baf84f8
Cooox1 <81319187+Cooox1@users.noreply.github.com>
2025-06-05 23:28:08 +0200

    06_05

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", " "]
 index=[ 0 , 1 , 2 ,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
@@ -32,16 +34,12 @@ def kodolas(uzenet, kulcs):
         for betu,szam in zip(abc,index):
             if k == szam:
                 kodolt += betu
-
+    
     return kodolt
-#print(kodolas(uzenet,kulcs))
-
+rejtjelezett = kodolas(uzenet,kulcs)
+#print(rejtjelezett)
 
 #DEKÓDOLÁS
-
-
-kod = "hfnosauzun"
-uzi = "curiosity killed the cat"
 
 def dekodolas(kodolt,uzenet):
     uzi_szam = []
@@ -68,4 +66,17 @@ def dekodolas(kodolt,uzenet):
                 kulcs += betu
 
     return kulcs
-print(dekodolas(kod,uzi))
+print(dekodolas(rejtjelezett,uzenet))
+
+
+#2
+
+
+uzenet1 = "" #early  4 0 17 11 24 
+uzenet2 = "" #curios 2 20 17 8 14 18
+kulcs = "" #
+with open("words.txt", "r", encoding="utf-8") as f:
+    mind = f.read()
+    szavak = list(map(str, mind.split()))
+
+    uzenet1 = random.choice(szavak)
--- test_main.py
+++ test_main.py
@@ -1,10 +1,9 @@
-from main import kodolas,dekodolas,abc,uzenet,kulcs, kodolt
+from main import kodolas,dekodolas,abc,index, uzenet,kulcs 
 import pytest
 
 # az üzenet az angol abc szerinti kisbetűkből (a-z) és szóközből (' ') állhat
 for betu in uzenet:
     assert betu in abc
-
 # a kulcs ugyanezeket a karaktereket tartalmazhatja
 for betu in kulcs:
     assert betu in abc
@@ -14,15 +13,43 @@ assert len(kulcs) >= len(uzenet)
 # rendeljünk a karakterhez egy kódot: a = 0, b = 1, ... z = 25, ' ' (szóköz) = 26
 
 # a rejtjelezett üzenet n. karaktere az üzenet n. karakter kódja + kulcs n. karakter kódja
+kodolt = kodolas(uzenet,kulcs)
+def test_kodolas():
+    uzenet_szam = [] 
+    kulcs_szam = []
+    for u, k in zip(uzenet,kulcs):
+        for betu,szam in zip(abc,index):
+            if u == betu:
+                uzenet_szam.append(szam)
+            if k == betu:
+                kulcs_szam.append(szam)
+    
+    kodolt = "" 
+    kodolt_szam = [] 
+    lista_hossza = len(index)
+    for u, k in zip(uzenet_szam, kulcs_szam):
+        # ha az eredmény nagyobb mint 26, akkor az eredmény a 27-el való osztás maradéka
+        kodolt_kar_szam = (u+k) % lista_hossza
+        if kodolt_kar_szam > 26:
+            assert kodolt_kar_szam == (u+k) % lista_hossza
+        kodolt_szam.append(kodolt_kar_szam)
+        
+    for k in kodolt_szam:
+        for betu,szam in zip(abc,index):
+            if k == szam:
+                kodolt += betu
 
-# ha az eredmény nagyobb mint 26, akkor az eredmény a 27-el való osztás maradéka
+    for u, k in zip(uzenet_szam, kulcs_szam):
+        for szam in kodolt_szam:
+            assert szam == u+k
+    
+
+        
 
 # ha a kulcs hosszabb mint az üzenet, akkor a rejtjeles üzenet hossza az üzenet hossza lesz
 def test_kodolas():
     if len(kulcs) > len(uzenet):
         assert len(kodolt) == len(uzenet)
-
-
 
 def test_kodolas():
     uzenet = "helloworld"
