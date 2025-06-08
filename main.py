72880921116ef2f15b95294a66634a10a627a3b2
Cooox1 <81319187+Cooox1@users.noreply.github.com>
2025-06-08 16:40:04 +0200

    második rész

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", " "]
 index=[ 0 , 1 , 2 ,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
@@ -66,17 +67,47 @@ def dekodolas(kodolt,uzenet):
                 kulcs += betu
 
     return kulcs
-print(dekodolas(rejtjelezett,uzenet))
-
+#print(dekodolas(rejtjelezett,uzenet))
 
 #2
 
-
-uzenet1 = "" #early  4 0 17 11 24 
-uzenet2 = "" #curios 2 20 17 8 14 18
-kulcs = "" #
+#Adatok generálása
+mind = []
 with open("words.txt", "r", encoding="utf-8") as f:
-    mind = f.read()
-    szavak = list(map(str, mind.split()))
+    for sor in f:
+        szo = sor.strip()
+        if szo:
+            mind.append(szo)
 
-    uzenet1 = random.choice(szavak)
+hossz1 = 3
+hossz2 = 3
+if len(mind) >= hossz1 + hossz2:
+        valasztott_szavak = random.sample(mind, hossz1 + hossz2)
+
+uzenetl1 = valasztott_szavak[hossz1:]
+uzenetl2 = valasztott_szavak[:hossz2]
+#--------Üzenetek stringgé alakítása--------------
+uzenet1 = ' '.join(uzenetl1)
+uzenet2 = ' '.join(uzenetl2)
+#----------Kulcs generálása-----------------
+length = len(uzenet1)
+kulcs = ''.join(random.choices(abc , k=length))
+
+#Kódolom az üzeneteket
+rejt_uzenet1 = kodolas(uzenet1,kulcs)
+rejt_uzenet2 = kodolas(uzenet2,kulcs)
+print(rejt_uzenet1)
+print(rejt_uzenet2)
+#binárisra lefordítom a stringeket
+def binarisra(szoveg):
+    return ''.join(format(ord(i), '08b') for i in szoveg)
+kulcs_bin = binarisra(kulcs)
+print(kulcs_bin)
+rejt_uzenet1_bin = binarisra(rejt_uzenet1)
+print(rejt_uzenet1_bin)
+rejt_uzenet2_bin = binarisra(rejt_uzenet2)
+print(rejt_uzenet2_bin)
+
+
+
+#kulcs(rész) = titkositott1(resz) + eredeti1(resz)
